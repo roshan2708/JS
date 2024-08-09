ec83918cc2de7a99243ce781a421919104e93c97
Roshan Singh <161965898+roshan2708@users.noreply.github.com>
2024-08-10 01:57:29 +0530

    Add files via upload

---
+++ data_types.js
@@ -0,0 +1,26 @@
+"use strict" //treat all js code as new version
+// alert (3+3) // we are using node js not browser
+let name = "Roshan Singh"
+let age = 21
+let email = "blablabla@femail.com"
+let number = 987654321
+let isLoggedIn=true
+console.table([name , age , email , number , isLoggedIn])
+console.table([typeof name , typeof age , typeof email , typeof number , typeof isLoggedIn])
+
+/* 
+notes
+number => 2 to the power 53 may be
+big int
+string => ""
+boolean = true//false
+null = standalone value
+undefined = undefined
+null is considered object
+symbol = unique
+*/
+let state
+let userId = null
+console.log(typeof state);
+console.log(typeof userId);
+
---
+++ variable.js
@@ -0,0 +1,7 @@
+const pi=3.14
+console.log(pi)
+console.log("Yo Bitch")
+let email="hellothere@gemale.com"
+// prefer not to use var due to issue in block scope and functional scope
+let number="number"
+console.table([email,number,pi])
