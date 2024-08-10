d7e4e46e2f7e0a20b119133e39c1e0aa99f40923
Roshan Singh <161965898+roshan2708@users.noreply.github.com>
2024-08-10 11:29:24 +0530

    Add files via upload

---
+++ conversionOperations.js
@@ -0,0 +1,19 @@
+let score = 55
+console.log(typeof score);
+let strScore = String(score)
+console.log(typeof strScore);
+let isloggedIn = 1
+let isLoggedIn2 = 0
+let isComing = "Nigga"
+let isStrong = ""
+let boolIsLoggedIn = Boolean(isloggedIn)
+let boolIsLoggedIn2 = Boolean(isLoggedIn2)
+let boolSsComing = Boolean(isComing)
+let boolIsStrong = Boolean(isStrong)
+console.log( boolIsLoggedIn );
+console.log(boolIsLoggedIn2 );
+console.log(boolSsComing );
+console.log(boolIsStrong);
+
+
+
