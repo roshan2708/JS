ac17abb155d7ff6bda714f95ecbd3e0eb6265ec2
Roshan Singh <161965898+roshan2708@users.noreply.github.com>
2024-08-12 18:18:10 +0000

    data_type_revision

---
+++ data_type2.js
@@ -0,0 +1,28 @@
+// PRIMITIVE
+// 7 TYPES : String , Number , Boolean . null , undefined , Symbol . Bigint
+
+const score = 55
+const someValue = 54
+const outsideTemp = null
+const name =" Roshan_Singh"
+let skinColour
+const something = Symbol("Nigga")
+const bigNumber = 12526965236n
+console.log(typeof bigNumber);
+console.log(typeof score);
+console.log(typeof someValue);
+console.log(typeof skinColour);
+console.log(typeof outsideTemp);
+console.log(typeof something);
+
+// Reference(Non-Primitive)
+//Array , Object , Functions
+const heroes = ["shaktimaan" , "doreman","nagraaj","ironman"]
+console.log(typeof heroes);
+
+
+
+
+
+
+
