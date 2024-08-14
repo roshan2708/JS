90924722413798fe5b6c1e2ea8ec2caaa0de1a59
Roshan Singh <161965898+roshan2708@users.noreply.github.com>
2024-08-14 15:56:09 +0000

    hods

--- strings.js
+++ strings.js
@@ -1,7 +1,7 @@
 const name = "Roshan"
 const num = 56
 console.log(`My name is ${name} and my count is ${num}`);
-
+// Methods in Strings
 const gameName = new String("NiggaMan")
 console.log(gameName);
 const yo = new String("Meow Meow Nigga")
@@ -11,6 +11,16 @@ console.log(yo.replace('Meow' , 'Kalua')); //replacing 1 element with other
 console.log(yo.replaceAll('Meow','Kalua'));//replacing when same element is present more than once
 console.log(yo.charAt(0));//character present at the index
 console.log(yo.endsWith("Nigga"));
+const arr=["hey","hello","bye","nigga","kinderGarden"]
+console.log(arr.slice(2,4));
+console.log(yo.toUpperCase());
+console.log(yo.toLowerCase());
+console.log(yo.substring(1,7));
+
+
+
+
+
 
 
 
