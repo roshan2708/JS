9ed6727e49990fca3ce581ed843d1ba7bf8befc9
Roshan Singh <161965898+roshan2708@users.noreply.github.com>
2024-08-14 07:06:17 +0000

    string methods

---
+++ strings.js
@@ -0,0 +1,22 @@
+const name = "Roshan"
+const num = 56
+console.log(`My name is ${name} and my count is ${num}`);
+
+const gameName = new String("NiggaMan")
+console.log(gameName);
+const yo = new String("Meow Meow Nigga")
+console.log(yo);
+console.log(yo.split(' ')); //splitting in form of an array
+console.log(yo.replace('Meow' , 'Kalua')); //replacing 1 element with other
+console.log(yo.replaceAll('Meow','Kalua'));//replacing when same element is present more than once
+console.log(yo.charAt(0));//character present at the index
+console.log(yo.charCodeAt(2:m));
+
+
+
+
+
+
+
+
+
