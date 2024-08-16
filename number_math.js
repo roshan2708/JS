ced4d6febde2594fa4a1b21f49548e09b6ac39e6
Roshan Singh <161965898+roshan2708@users.noreply.github.com>
2024-08-16 03:40:01 +0000

    math functions

--- number_math.js
+++ number_math.js
@@ -1,7 +1,30 @@
-const score = 95
-const num =Number("58")
-console.log(typeof score);
-console.log(typeof num);
+// const score = 95.26552
+// const num =Number(58)
+// console.log(typeof score);
+// console.log(typeof num);
+// console.log(score.toPrecision(4))
+
+// const num2 = 1000000000
+// const newNum = num2.toString
+// console.log(newNum);
+
+
+/********************************************************** */
+
+console.log(Math.PI.toPrecision(5));
+console.log(Math.sin(0));
+console.log(Math.cos(0));
+console.log(Math.exp(0));
+
+
+
+
+
+
+
+
+
+
 
 
 
